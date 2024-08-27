fc9693c38481d8922673fe91e6a4115982667762
胡一刀 <codeshow2024@163.com>
2024-08-27 11:40:45 +0800

    feat:添加日志打印

--- common/src/main/java/top/codeshow/common/util/MyStrUtils.java
+++ common/src/main/java/top/codeshow/common/util/MyStrUtils.java
@@ -10,15 +10,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MyStrUtils {
     /**
-     * 加密密码
+     * 是否为空字符串
      *
-     * @param oldPassword 原密码
-     * @return 加密过的密码
+     * @param str 字符串
+     * @return
      */
-    public static String encryptPassword(String oldPassword) {
-        return SecureUtil.md5(oldPassword);
+    public static boolean isBlank(String str) {
+        return null == str || str.trim().isEmpty();
     }
 
+    public static boolean isNotBlank(String str) {
+        return !isBlank(str);
+    }
 
     /**
      * 生成 uuid
@@ -28,4 +31,15 @@ public class MyStrUtils {
     public static String getUuid() {
         return IdUtil.fastSimpleUUID();
     }
+
+    /**
+     * 加密密码
+     *
+     * @param oldPassword 原密码
+     * @return 加密过的密码
+     */
+    public static String encryptPassword(String oldPassword) {
+        return SecureUtil.md5(oldPassword);
+    }
+
 }
